a9c7fc5d753631e021eb8da335b567d5ba9091fd
João Guilherme <joaaogui@gmail.com>
2020-01-12 14:12:39 -0300

    Creating components

---
+++ src/components/Tables.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Tables</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tables'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Pdv.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>sdasdasd</h1>
+    <Tables/>
+  </div>
+</template>
+
+<script>
+import Tables from '../components/Tables'
+
+export default {
+  name: 'Pdv',
+  components: { Tables }
+}
+</script>
+
+<style scoped>
+
+</style>
