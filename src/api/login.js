9633d8d77028934b9330246358ab966b70400487
João Guilherme <joaaogui@gmail.com>
2020-02-28 13:51:48 -0300

    Updating endpoints

---
+++ src/api/login.js
@@ -0,0 +1,24 @@
+import axiosInstance from "axios"
+
+const axios = axiosInstance.create({
+  baseURL: process.env.VUE_APP_BASE_URL,
+})
+
+
+const login = ({input, password}) => axios.get(`https://kadde-dev-api.azurewebsites.net/api/autorizacao/verificar?email=${input}&senha=${password}`)
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
+function establishmentLogin(loginData){
+  const searchParams = new URLSearchParams()
+  for (const prop in loginData) {
+    searchParams.set(prop, loginData[prop])
+  }
+  return axios.post(`https://kadde-dev-auth.azurewebsites.net/api/oauth2/token`, searchParams, {
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+  })
+}
+
+export {
+  login,
+  establishmentLogin
+}
---
+++ src/components/Establishments.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="centered-container">
+    <v-autocomplete
+      v-model="establishment"
+      :items="establishments"
+      item-text="nome"
+      item-value="id"
+      label="Estabelecimentos"
+      @change="setEstablishment"
+    ></v-autocomplete>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Establishments",
+    data: () => ({
+      establishment: null
+    }),
+    computed: {
+      establishments() {
+        return this.$store.state.establishments
+      }
+    },
+    methods: {
+      setEstablishment() {
+        let data = {
+          username: this.$route.params.input,
+          password: this.$route.params.password,
+          origem: 1,
+          grant_type: "password",
+          loja_id: this.establishment,
+          client_id: process.env.VUE_APP_CLIENT_ID
+        }
+
+        this.$store.commit('setEstablishment', this.establishment)
+        this.$store.dispatch('establishmentLogin', data)
+        this.$router.push('/pdv')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+-
